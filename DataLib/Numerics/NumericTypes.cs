53de1063d545dfef84dbdb165b0d9822935047ac
Alain Barthe <alain@barthe.net>
2021-01-29 21:15:18 +0100

    Adding numerics

---
+++ DataLib/Numerics/NumericTypes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLib.Numerics
+{
+    public static class NumericTypes
+    {
+        public static Type[] SupportedTypes { get; } = 
+        { 
+            typeof(byte), 
+            typeof(Int16), 
+            typeof(Int32), 
+            typeof(Int64), 
+            typeof(float), 
+            typeof(double), 
+            typeof(decimal) 
+        };
+
+        public static Dictionary<Type, int> TypeMap { get; } = new Dictionary<Type, int>();
+
+        public static object Upgrade(object n, Type toType)
+        {
+            var si = GetTypeIndex(n);
+            var ti = GetTypeIndex(toType);
+            if (si > ti)
+                throw new Exception("Cannot upgrade to lesser type");
+            return si != ti ? Convert.ChangeType(n, Type.GetTypeCode(SupportedTypes[ti])) : n;
+        }
+
+        public static bool MakeSame(ref object a, ref object b)
+        {
+            var ta = GetTypeIndex(a);
+            var tb = GetTypeIndex(b);
+            if (ta == tb)
+            {
+                return false;
+            }
+            else if (ta > tb)
+            {
+                b = Upgrade(b, SupportedTypes[ta]);
+                return true;
+            }
+            else
+            {
+                a = Upgrade(a, SupportedTypes[tb]);
+                return true;
+            }
+        }
+
+        static int GetTypeIndex(object o)
+        {
+            return GetTypeIndex(o.GetType());
+        }
+
+        static int GetTypeIndex(Type t)
+        {
+            if (!TypeMap.ContainsKey(t))
+                throw new Exception("Unsupported type: " + t.Name);
+            return TypeMap[t];
+        }
+
+        static NumericTypes()
+        {
+            for (var i = 0; i < SupportedTypes.Length; i++)
+                TypeMap.Add(SupportedTypes[i], i);
+        }
+    }
+}
---
+++ UnitTests/Numerical.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLib.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Numerical
+    {
+        [TestMethod]
+        public void Basics()
+        {
+            object a = (byte)1;
+            object b = (int)2;
+
+            Assert.IsTrue(NumericTypes.MakeSame(ref a, ref b));
+            Assert.AreEqual(typeof(int), a.GetType());
+            Assert.AreEqual(typeof(int), b.GetType());
+            Assert.AreEqual(1, a);
+            Assert.AreEqual(2, b);
+        }
+    }
+}
